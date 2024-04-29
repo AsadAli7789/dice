c18f599356d6e60d3bcfaa6399dfb18881f64f94
AsadAli7789 <155202559+AsadAli7789@users.noreply.github.com>
2024-04-29 11:54:02 -0700

    done

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+document.getElementById("mol")
+function va1l() {
+    var dow = document.getElementById("po3").innerHTML = Math.ceil(Math.random() * 6);
+    var b1 = 1
+    document.getElementById("pof").innerHTML = "1"
+
+    if (
+        b1 == dow
+    ) {document.getElementById("mol").innerHTML = "you are the winner"
+
+    }
+    else {document.getElementById("mol").innerHTML = "you are the losser"
+
+    }
+
+}
+
+
+function va2l() {
+    var dow = document.getElementById("po3").innerHTML = Math.ceil(Math.random() * 6);
+    var b2 = 2
+    document.getElementById("pof").innerHTML = "2"
+    if (b2 == dow) { document.getElementById("mol").innerHTML = "you are the winner" }
+    else { document.getElementById("mol").innerHTML = "you are the losser" }
+}
+function va3l() {
+    var dow = document.getElementById("po3").innerHTML = Math.ceil(Math.random() * 6);
+    var b3 = 3
+    document.getElementById("pof").innerHTML = "3"
+    if (b3 == dow) { document.getElementById("mol").innerHTML = "you are the winner" }
+    else { document.getElementById("mol").innerHTML = "you are the losser" }
+}
+function va4l() {
+    var dow = document.getElementById("po3").innerHTML = Math.ceil(Math.random() * 6);
+    var b4 = 4
+    document.getElementById("pof").innerHTML = "4"
+    if (b4 == dow) { document.getElementById("mol").innerHTML = "you are the winner" }
+    else { document.getElementById("mol").innerHTML = "you are the losser" }
+}
+function va5l() {
+    var dow = document.getElementById("po3").innerHTML = Math.ceil(Math.random() * 6);
+    var b5 = 5
+    document.getElementById("pof").innerHTML = "5"
+    if (b5 == dow) { document.getElementById("mol").innerHTML = "you are the winner" }
+    else { document.getElementById("mol").innerHTML = "you are the losser" }
+}
+function va6l() {
+    var dow = document.getElementById("po3").innerHTML = Math.ceil(Math.random() * 6);
+    var b6 = 6
+    document.getElementById("pof").innerHTML = "6"
+    if (b6 == dow) { document.getElementById("mol").innerHTML = "you are the winner" }
+    else { document.getElementById("mol").innerHTML = "you are the losser" }
+}
+
+
